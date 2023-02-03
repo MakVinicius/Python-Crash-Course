6086731194088fd445119c15fe085a6201c631d4
Mak Vinícius <mvgj369@gmail.com>
2023-02-03 16:33:44 -0300

    Capítulo 2
    Alguns exercícios do capítulo 2.

---
+++ chapter_2/name.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
---
+++ chapter_2/personal_message.py
@@ -0,0 +1,12 @@
+persons_name = "michael morson"
+print(f"Hello {persons_name.title()}! Welcome to python crash course!")
+
+persons_name = "michael morson"
+print(persons_name.title())
+print(persons_name.lower())
+print(persons_name.upper())
+
+famous_person = "albert einstein"
+message = f'{famous_person.title()} once said, "A person who never made a mistake never tried anything new."'
+print(message)
+#exercises from the book
